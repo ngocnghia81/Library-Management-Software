5ac9bc1bd4c325f968577128e07169f356a5ceb8
Betaa04 <khanhsoai6@gmail.com>
2024-10-21 19:30:31 +0700

    formDangNhap

--- LibraryManagementSoftware/frmDangNhap.cs
+++ LibraryManagementSoftware/frmDangNhap.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace LibraryManagementSoftware
 {
@@ -15,6 +16,39 @@ namespace LibraryManagementSoftware
         public frmDangNhap()
         {
             InitializeComponent();
+        }
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void txtEmail_Enter(object sender, EventArgs e)
+        {
+            if (txtEmail.Text != "Email") return;
+            txtEmail.ForeColor = Color.Black;
+            txtEmail.Text = "";
+        }
+
+        private void txtEmail_Leave(object sender, EventArgs e)
+        {
+            
+            if (txtEmail.Text != "") return;
+            txtEmail.ForeColor = Color.LightGray;
+            txtEmail.Text = "Email";
+        }
+
+        private void txtPassword_Enter(object sender, EventArgs e)
+        {
+            if (txtPassword.Text != "Password") return;
+            txtPassword.ForeColor = Color.Black;
+            txtPassword.Text = "";
+        }
+
+        private void txtPassword_Leave(object sender, EventArgs e)
+        {
+            if (txtPassword.Text != "") return;
+            txtPassword.ForeColor = Color.LightGray;
+            txtPassword.Text = "Password";
         }
     }
 }
