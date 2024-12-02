16b6d279529363ef4e84d9415fd6500e9d5264b7
Betaa04 <khanhsoai6@gmail.com>
2024-12-02 13:17:13 +0700

    tra sach

--- LibraryManagementSoftware/frmTraSach.Designer.cs
+++ LibraryManagementSoftware/frmTraSach.Designer.cs
@@ -28,70 +28,144 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
             this.cbbDocGias = new System.Windows.Forms.ComboBox();
-            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.dgv1 = new System.Windows.Forms.DataGridView();
+            this.btnLuu = new Guna.UI2.WinForms.Guna2Button();
+            this.dgv1 = new Guna.UI2.WinForms.Guna2DataGridView();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cbbSach = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.dgv1)).BeginInit();
             this.SuspendLayout();
             // 
             // cbbDocGias
             // 
             this.cbbDocGias.FormattingEnabled = true;
-            this.cbbDocGias.Location = new System.Drawing.Point(139, 27);
+            this.cbbDocGias.Location = new System.Drawing.Point(124, 13);
+            this.cbbDocGias.Margin = new System.Windows.Forms.Padding(4);
             this.cbbDocGias.Name = "cbbDocGias";
-            this.cbbDocGias.Size = new System.Drawing.Size(186, 24);
+            this.cbbDocGias.Size = new System.Drawing.Size(232, 28);
             this.cbbDocGias.TabIndex = 1;
             this.cbbDocGias.SelectedIndexChanged += new System.EventHandler(this.cbbDocGias_SelectedIndexChanged);
-            // 
-            // dateTimePicker1
-            // 
-            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.dateTimePicker1.Location = new System.Drawing.Point(468, 29);
-            this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(200, 22);
-            this.dateTimePicker1.TabIndex = 2;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.label1.Location = new System.Drawing.Point(1, 31);
+            this.label1.Location = new System.Drawing.Point(1, 12);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(91, 20);
+            this.label1.Size = new System.Drawing.Size(115, 25);
             this.label1.TabIndex = 3;
             this.label1.Text = "Tên độc giả";
             // 
-            // label2
+            // btnLuu
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.label2.Location = new System.Drawing.Point(332, 31);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(89, 20);
-            this.label2.TabIndex = 4;
-            this.label2.Text = "Ngày mượn";
+            this.btnLuu.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btnLuu.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnLuu.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnLuu.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnLuu.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnLuu.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.btnLuu.ForeColor = System.Drawing.Color.White;
+            this.btnLuu.Location = new System.Drawing.Point(855, 13);
+            this.btnLuu.Margin = new System.Windows.Forms.Padding(4);
+            this.btnLuu.Name = "btnLuu";
+            this.btnLuu.Size = new System.Drawing.Size(134, 54);
+            this.btnLuu.TabIndex = 6;
+            this.btnLuu.Text = "Lưu";
+            this.btnLuu.Click += new System.EventHandler(this.btnLuu_Click);
             // 
             // dgv1
             // 
+            this.dgv1.AllowUserToAddRows = false;
+            this.dgv1.AllowUserToDeleteRows = false;
+            dataGridViewCellStyle4.BackColor = System.Drawing.Color.White;
+            this.dgv1.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle4;
+            this.dgv1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            dataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            dataGridViewCellStyle5.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle5.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle5.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle5.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgv1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle5;
             this.dgv1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgv1.Location = new System.Drawing.Point(12, 137);
+            dataGridViewCellStyle6.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle6.BackColor = System.Drawing.Color.White;
+            dataGridViewCellStyle6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            dataGridViewCellStyle6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            dataGridViewCellStyle6.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle6.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            dataGridViewCellStyle6.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgv1.DefaultCellStyle = dataGridViewCellStyle6;
+            this.dgv1.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
+            this.dgv1.Location = new System.Drawing.Point(12, 116);
             this.dgv1.Name = "dgv1";
+            this.dgv1.RowHeadersVisible = false;
+            this.dgv1.RowHeadersWidth = 51;
             this.dgv1.RowTemplate.Height = 24;
-            this.dgv1.Size = new System.Drawing.Size(748, 412);
-            this.dgv1.TabIndex = 0;
+            this.dgv1.Size = new System.Drawing.Size(978, 501);
+            this.dgv1.TabIndex = 7;
+            this.dgv1.ThemeStyle.AlternatingRowsStyle.BackColor = System.Drawing.Color.White;
+            this.dgv1.ThemeStyle.AlternatingRowsStyle.Font = null;
+            this.dgv1.ThemeStyle.AlternatingRowsStyle.ForeColor = System.Drawing.Color.Empty;
+            this.dgv1.ThemeStyle.AlternatingRowsStyle.SelectionBackColor = System.Drawing.Color.Empty;
+            this.dgv1.ThemeStyle.AlternatingRowsStyle.SelectionForeColor = System.Drawing.Color.Empty;
+            this.dgv1.ThemeStyle.BackColor = System.Drawing.Color.White;
+            this.dgv1.ThemeStyle.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
+            this.dgv1.ThemeStyle.HeaderStyle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
+            this.dgv1.ThemeStyle.HeaderStyle.BorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            this.dgv1.ThemeStyle.HeaderStyle.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.dgv1.ThemeStyle.HeaderStyle.ForeColor = System.Drawing.Color.White;
+            this.dgv1.ThemeStyle.HeaderStyle.HeaightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv1.ThemeStyle.HeaderStyle.Height = 22;
+            this.dgv1.ThemeStyle.ReadOnly = false;
+            this.dgv1.ThemeStyle.RowsStyle.BackColor = System.Drawing.Color.White;
+            this.dgv1.ThemeStyle.RowsStyle.BorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.SingleHorizontal;
+            this.dgv1.ThemeStyle.RowsStyle.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.dgv1.ThemeStyle.RowsStyle.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            this.dgv1.ThemeStyle.RowsStyle.Height = 24;
+            this.dgv1.ThemeStyle.RowsStyle.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
+            this.dgv1.ThemeStyle.RowsStyle.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label3.Location = new System.Drawing.Point(380, 12);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(98, 25);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Tên Sách";
+            // 
+            // cbbSach
+            // 
+            this.cbbSach.FormattingEnabled = true;
+            this.cbbSach.Location = new System.Drawing.Point(503, 13);
+            this.cbbSach.Margin = new System.Windows.Forms.Padding(4);
+            this.cbbSach.Name = "cbbSach";
+            this.cbbSach.Size = new System.Drawing.Size(232, 28);
+            this.cbbSach.TabIndex = 8;
+            this.cbbSach.SelectedIndexChanged += new System.EventHandler(this.cbbDocGias_SelectedIndexChanged);
             // 
             // frmTraSach
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(784, 561);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.dateTimePicker1);
-            this.Controls.Add(this.cbbDocGias);
+            this.ClientSize = new System.Drawing.Size(1002, 644);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cbbSach);
             this.Controls.Add(this.dgv1);
+            this.Controls.Add(this.btnLuu);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbbDocGias);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "frmTraSach";
             this.Text = "frmTraSach";
             this.Load += new System.EventHandler(this.frmTraSach_Load);
@@ -103,9 +177,10 @@
 
         #endregion
         private System.Windows.Forms.ComboBox cbbDocGias;
-        private System.Windows.Forms.DateTimePicker dateTimePicker1;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.DataGridView dgv1;
+        private Guna.UI2.WinForms.Guna2Button btnLuu;
+        private Guna.UI2.WinForms.Guna2DataGridView dgv1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cbbSach;
     }
 }
--- LibraryManagementSoftware/frmTraSach.cs
+++ LibraryManagementSoftware/frmTraSach.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -13,51 +15,100 @@ namespace LibraryManagementSoftware
     public partial class frmTraSach : Form
     {
         DBConnection db = new DBConnection();
+
+        public string TaoMaPT()
+        {
+            string ma = db.ExecuteScalar("select max(maphieutra) from  phieutra").ToString();
+            int phanSo = int.Parse(ma.Substring(2));
+            phanSo++;
+            ma = "PT" + phanSo.ToString("D3");
+            return ma;
+        }
+
+        float TinhLePhi(string ngaymuon)
+        {
+            DateTime _ngaymuon = DateTime.Parse(ngaymuon);
+            int songaymuon = (DateTime.Now- _ngaymuon).Days;
+            if (songaymuon <= 7)
+                return 0;
+            if (songaymuon <= 15)
+                return 10000;
+            return 50000;
+        }
         void LoadDocGia()
         {
             string query = "select * from DocGia";
             DataTable dt = db.ExecuteSelect(query);
-
-            cbbDocGias.DataSource = dt;
+            DataRow row = dt.NewRow();
+            row["MaDocGia"] = "";
+            row["TenDocGia"] = "Tất cả độc giả";
+            dt.Rows.InsertAt(row, 0);
+            cbbDocGias.DataSource = dt;          
             cbbDocGias.DisplayMember = "TenDocGia";
             cbbDocGias.ValueMember = "MaDocGia";
-
-            cbbDocGias.SelectedIndex = 0;
         }
 
-        void LoadDgv(string query)
+        public void LoadSach()
         {
-
+            string query = "select * from Sach";
             DataTable dt = db.ExecuteSelect(query);
+            DataRow row = dt.NewRow();
+            row["MaSach"] = "";
+            row["TenSach"] = "Tất cả sách";
+            dt.Rows.InsertAt(row, 0);
+            cbbSach.DataSource = dt;
+            cbbSach.DisplayMember = "TenSach";
+            cbbSach.ValueMember = "MaSach";
+        }
+        void LoadDgv(string madg = null,string masach = null)
+        {
+            if (madg == null || madg == "")
+                madg = "null"; 
+            else madg = "'" + madg + "'";
 
-            DataGridViewCheckBoxColumn chkColumn = new DataGridViewCheckBoxColumn();
-            chkColumn.Name = "TinhTrangCheckbox";
-            chkColumn.HeaderText = "Tình Trạng";
-            chkColumn.TrueValue = true;
-            chkColumn.FalseValue = false;
+            if (masach == null || masach == "")
+                masach = "null";
+            else masach = "'" + masach + "'";
+
+            string sql = string.Format("select * from dbo.DANHSACHMUONTRA({0},{1}) where TinhTrang = 0",madg,masach);
+
+            DataTable dt = db.ExecuteSelect(sql); 
+
 
             dgv1.DataSource = dt;
-
-            dgv1.Columns.Add(chkColumn);
-
-            foreach (DataGridViewRow row in dgv1.Rows)
-            {
-                string tinhTrang = row.Cells["TinhTrang"].Value?.ToString();
-                row.Cells["TinhTrangCheckbox"].Value = (tinhTrang == "Đã trả");
-            }
-
-            dgv1.Columns.Remove("TinhTrang");
-
+            dgv1.Columns["MaDG"].HeaderText = "Mã độc giả";
+            dgv1.Columns["TenDG"].HeaderText = "Tên độc giả";
+            dgv1.Columns["TenSach"].HeaderText = "Sách";
+            dgv1.Columns["MaPhieuMuon"].HeaderText = "Mã phiếu mượn";
+            dgv1.Columns["NgayMuon"].HeaderText = "Ngày mượn";
+            dgv1.Columns["TinhTrang"].HeaderText = "Tình trạng";
+            dgv1.Columns["MaCTPM"].Visible = false;
             foreach (DataGridViewColumn column in dgv1.Columns)
             {
-                if (column.Name != "TinhTrangCheckbox")
+                if (column.HeaderText != "Tình trạng") // "TinhTrang" là tên cột cần chỉnh sửa
                 {
                     column.ReadOnly = true;
+                    continue;
                 }
             }
+            if (dgv1.Columns["TinhTrangBanDau"] == null)
+            {
+                dgv1.Columns.Add("TinhTrangBanDau", "TinhTrangBanDau"); // Thêm cột
+                dgv1.Columns["TinhTrangBanDau"].Visible = false;
+            }                
+            foreach (DataGridViewRow row in dgv1.Rows)
+            {
+                row.Cells["TinhTrangBanDau"].Value = row.Cells["TinhTrang"].Value; // Lưu trạng thái ban đầu
+            }
+            foreach (DataGridViewRow row in dgv1.Rows)
+            {
+                bool tinhTrang = Convert.ToBoolean(row.Cells["TinhTrang"].Value); // Lấy giá trị cột "Tình trạng"
 
-            dgv1.Columns["TinhTrangCheckbox"].ReadOnly = false;
-
+                if (tinhTrang) // Nếu đã check (true)
+                {
+                    row.Cells["TinhTrang"].ReadOnly = true;
+                }
+            }
         }
 
         public frmTraSach()
@@ -67,15 +118,50 @@ namespace LibraryManagementSoftware
 
         private void frmTraSach_Load(object sender, EventArgs e)
         {
+            LoadSach();
             LoadDocGia();
-            LoadDgv("select * from ChiTietPhieuMuon");
+            LoadDgv();
+        
         }
 
         private void cbbDocGias_SelectedIndexChanged(object sender, EventArgs e)
         {
-            string query = string.Format("select * from ChiTietPhieuMuon where MaDocGia = '{0}'",cbbDocGias.SelectedValue.ToString());
+            if (cbbDocGias.SelectedValue == null) return;
+            if (cbbSach.SelectedValue == null) return;
+            string madg = cbbDocGias.SelectedValue.ToString();
+            string masach = cbbSach.SelectedValue.ToString();
+            LoadDgv(madg,masach);
 
-            //LoadDgv(query);
+        }
+
+        private void btnLuu_Click(object sender, EventArgs e)
+        {
+            foreach (DataGridViewRow row in dgv1.Rows)
+            {
+                if (row.IsNewRow) continue;
+               
+                bool isChecked = Convert.ToBoolean(row.Cells["TinhTrang"].Value);
+                bool bandau = Convert.ToBoolean(row.Cells["TinhTrangBanDau"].Value);
+
+                if (isChecked != bandau && isChecked)
+                {                   
+                    string maDG = row.Cells["MaDG"].Value.ToString();
+                    string maCTPM = row.Cells["MaCTPM"].Value.ToString();
+
+                    string ngaymuon = row.Cells["NgayMuon"].Value.ToString();
+                    string sql = "INSERT INTO PhieuTra (MaPhieuTra, NgayTra, LePhi, MaCTPM) VALUES (@MaPhieuTra, GETDATE(), @LePhi, @MaCTPM)";
+                    SqlParameter[] parameters = new SqlParameter[]
+                    {
+                        new SqlParameter("@MaPhieuTra", TaoMaPT()),
+                        new SqlParameter("@LePhi", TinhLePhi(ngaymuon)),
+                        new SqlParameter("@MaCTPM", maCTPM)
+                    };
+                    db.ExecuteInsert(sql, parameters);
+
+                }
+                
+            }
+            LoadDgv();
 
         }
     }
