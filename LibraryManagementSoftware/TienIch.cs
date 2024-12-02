3ab6f2f3ba604020531baa3e533d06fb89d01855
ngocnghia81 <ngocnghia.workspace@gmail.com>
2024-12-02 13:26:33 +0700

    frmMuonSach

--- LibraryManagementSoftware/TienIch.cs
+++ LibraryManagementSoftware/TienIch.cs
@@ -11,21 +11,21 @@ namespace LibraryManagementSoftware
         internal static class TienIch
         {
 
-        public static void GoiYComboBox(ComboBox comboBox, string displayMember)
-        {
-            comboBox.AutoCompleteMode = AutoCompleteMode.SuggestAppend;
-            comboBox.AutoCompleteSource = AutoCompleteSource.CustomSource;
-            AutoCompleteStringCollection autoCompleteCollection = new AutoCompleteStringCollection();
-
-            foreach (DataRowView item in comboBox.Items)
+            public static void GoiYComboBox(ComboBox comboBox, string displayMember)
             {
-                if (!item.Row.ItemArray[0].ToString().Equals("ADD", StringComparison.OrdinalIgnoreCase))
-                {
-                    autoCompleteCollection.Add(item[displayMember].ToString());
-                }
-            }
+                comboBox.AutoCompleteMode = AutoCompleteMode.SuggestAppend;
+                comboBox.AutoCompleteSource = AutoCompleteSource.CustomSource;
+                AutoCompleteStringCollection autoCompleteCollection = new AutoCompleteStringCollection();
 
-            comboBox.AutoCompleteCustomSource = autoCompleteCollection;
-        }
+                foreach (DataRowView item in comboBox.Items)
+                {
+                    if (!item.Row.ItemArray[0].ToString().Equals("ADD", StringComparison.OrdinalIgnoreCase))
+                    {
+                        autoCompleteCollection.Add(item[displayMember].ToString());
+                    }
+                }
+
+                comboBox.AutoCompleteCustomSource = autoCompleteCollection;
+            }
     }
 }
